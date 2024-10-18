47000872f73aef10b605416a82953ba47b48a9e3
KEVIN ALONSO MAMANI CONDORI <kmamanicondo@unsa.edu.pe>
2024-10-18 18:44:22 -0500

    Subiendo proyecto

---
+++ app/src/main/java/com/example/lab05_layouts_widgets/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.lab05_layouts_widgets;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.lab05_layouts_widgets.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnCAQP.setOnClickListener( v -> {
+            loadFragment(new CatedralFragment());
+        });
+
+        binding.btnCC.setOnClickListener( v -> {
+            loadFragment(new ClaustoFragment());
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+}
